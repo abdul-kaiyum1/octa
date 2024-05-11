501953e27ec5c8f34f4fdff0fe7ef45600147e18
abdul-kaiyum1 <131800826+abdul-kaiyum1@users.noreply.github.com>
2024-05-11 18:09:21 +0600

    Update rank2.js

--- scripts/cmds/rank2.js
+++ scripts/cmds/rank2.js
@@ -80,7 +80,7 @@ module.exports = {
 const defaultDesignCard = {
 	widthCard: 2000,
 	heightCard: 500,
-	main_color: "https://imgur.com/a/lQYJhCb",
+	main_color: "https://i.ibb.co/g7cCj1g/Subheading.png",
 	sub_color: "rgba(255, 255, 255, 0.5)",
 	alpha_subcard: 0.0,
 	exp_color: "#e1e1e1",
