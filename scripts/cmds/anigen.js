04af9e0b245a7bca7ff8c105e571d94a03845c7b
abdul-kaiyum1 <131800826+abdul-kaiyum1@users.noreply.github.com>
2024-08-01 18:42:37 +0600

    Update anigen.js

--- scripts/cmds/anigen.js
+++ scripts/cmds/anigen.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 global.api = {
-  samirApi: "https://samirxpikachu.onrender.com"
+  samirApi: "https://www.samirxpikachu.run.place"
 };
 
 module.exports = {
